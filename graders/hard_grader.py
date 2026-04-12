d967844bbb054df24ba4c7b1a74e1a82e70c15d7
mohamedsafir <119931182+mohamedsafir@users.noreply.github.com>
2026-04-12 22:21:43 +0530

    Update hard_grader.py

--- graders/hard_grader.py
+++ graders/hard_grader.py
@@ -1,9 +1,9 @@
 def grade_hard(output, task):
-    output = output.replace(" ", "")
+    output = str(output).replace(" ", "")
 
     if "a/b" in output:
-        return 1.0
+        return 0.9
     elif "/" in output:
         return 0.5
     else:
-        return 0.0
+        return 0.1  # NOT 0.0
